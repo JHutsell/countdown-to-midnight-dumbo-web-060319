76ca9312b9ee8e446e77d5a977f0113b1c5a35f9
Justin Hutsell <hutselljustin@gmail.com>
2019-05-29 19:29:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,11 @@ def countdown(seconds_left)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds_left)
+  while seconds_left > 0 
+    puts "#{seconds_left} SECOND(S)!"
+    seconds_left -= 1 
+    sleep
+  end
+end
